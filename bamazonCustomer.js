9019cec6161bb31eeb4430690b2b354f60d41ebf
Xerdet <lurobper@gmail.com>
2019-02-21 16:32:14 -0600

    Documented both of the bamazon app files

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,11 @@
+//Initializes the two node modules that will be used throughout the application
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var divider = "==================================================\n";
 
+//Creates a divider for aesthetics
+var divider = "========================================================\n";
+
+//Initializes the SQL database connection
 var connection = mysql.createConnection({
   host: "localhost",
 
@@ -16,6 +20,7 @@ var connection = mysql.createConnection({
   database: "bamazondb"
 });
 
+//Function that connects to the database and initializes the Bamazon Customer app
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
@@ -24,22 +29,27 @@ connection.connect(function(err) {
 
 })
 
+//Function that displays a welcome message upon being called
 function dispWelcome()
 {
   console.log(divider);
-  console.log("===============Welcome to Bamazon!===============\n");
-  console.log("Please select the item ID from the list below\n")
-  console.log("for any item you wish to purchase. Then select\n");
-  console.log("the quantity you wish to purchase from our stock\n");
+  console.log("==================Welcome to Bamazon!===================\n");
+  console.log("|    Please select the item ID from the list below     |")
+  console.log("|    for any item you wish to purchase. Then select    |");
+  console.log("|    the quantity you wish to purchase from our stock  |\n");
   console.log(divider);
 }
 
+//Main function of the app. Will display all the products currently in stock and allow the user to select one to purchase
 function bamazon()
 {
+  //Connects to the database in order to obtain a list of all the products in stock
   connection.query("SELECT * FROM products", function(err,res){
       if (err) throw err;
       var stock = res;
+  //Calls the display function in order to show the user all the items in stock
       dispStock(stock);
+  //Prompts the user to input an item id and then a quantity they wish to purchase
       inquirer.prompt([
         {
           type: "input",
@@ -52,19 +62,24 @@ function bamazon()
             message: "How many would you like to purchase? "
         }
     ]).then(function(answer){
+    
+    //Once the user makes a selection the app will verify if it's a valid item id. If it isn't then the connection will end
       if(answer.item < stock.length+1 && answer.item > 0)
       {
+       //If the item id selected by the user is valid then the app verifies if there are enough items in stock to fulfill the order
+       //if there aren't then the connection ends
        var item_pos = parseInt(answer.item) - 1;
        var availableStock = parseInt(stock[item_pos].stock_quantity);
        if(availableStock > 0 && answer.amount <= availableStock)
        {
+         //If there are enough items in stock to fulfill the order then the purchase is completed. 
          var newStock = availableStock - parseInt(answer.amount);
          var totalCost = stock[item_pos].price * answer.amount;
-
+         //The product's current stock is updated on the corresponding table
          connection.query("UPDATE products SET stock_quantity=? WHERE item_id =?",[newStock,answer.item], function(err){
          
         if (err) throw err;
-
+         //The user is informed that their purchase went through and is shown the total cost of all the items.
          console.log(divider);
          console.log("Purchase has been made.");
          console.log("Your total cost was: $" + totalCost);
@@ -74,20 +89,21 @@ function bamazon()
          })
        }else{
         console.log(divider);
-        console.log("Insufficient Quantity! Please select a different item.\n");
+        console.log("Insufficient Quantity! Please try again and select a different item\n");
         console.log(divider);
-        bamazon();
+        connection.end();
        }
     }else{
         console.log(divider);
-        console.log("Incorrect Item! Please select another item.\n");
+        console.log("Incorrect Item! Please try again and select another item\n");
         console.log(divider);
-        bamazon();
+        connection.end();
     }
     })
   })
 };
 
+//Function that cycles through all the stock and displays it for the user
 function dispStock(x)
 {
     console.log("Item ID" + " || " + "Product Name"+ " || " + "Price" + " || "  + "Quantity Available");
--- bamazonManager.js
+++ bamazonManager.js
@@ -1,7 +1,11 @@
+//Initializes the two node modules that will be used throughout the application
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var divider = "==================================================\n";
 
+//Creates a divider for aesthetics
+var divider = "========================================================\n";
+
+//Initializes the SQL database connection
 var connection = mysql.createConnection({
   host: "localhost",
 
@@ -16,6 +20,7 @@ var connection = mysql.createConnection({
   database: "bamazondb"
 });
 
+//Function that connects to the database and initializes the Bamazon Manager app
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
@@ -24,6 +29,7 @@ connection.connect(function(err) {
 
 })
 
+//Main function that offers the manager several options and functions that can be performed
 function manager()
 {
  connection.query("SELECT * FROM products", function(err,res)
@@ -38,6 +44,7 @@ function manager()
           choices: ["View Products for Sale","View Low Inventory","Add to Inventory","Add New Product","Exit"]
         }
     ]).then(function(reply){
+      //Based on the user's decision, the function will cycle through the switch statement and call the appropriate function
        switch(reply.decision)
        {
          case "View Products for Sale":
@@ -59,6 +66,7 @@ function manager()
          case "Add New Product":
          addProduct();
          break;
+        //However if the user wishes to exit then the connection will be terminated
          case "Exit":
          connection.end();
          break
@@ -67,15 +75,17 @@ function manager()
   })
 }
 
+//Function that displays a welcome message upon being called
 function dispWelcome()
 {
   console.log(divider);
-  console.log("=======Welcome to the Bamazon Manager Menu=======\n");
-  console.log("Please select one of the options from the menu below\n")
-  console.log("in order to update and manage the items in stock\n");
+  console.log("==========Welcome to the Bamazon Manager Menu===========\n");
+  console.log("| Please select one of the options from the menu below |\n")
+  console.log("| in order to update and manage the items in stock     |\n");
   console.log(divider);
 }
 
+//Function that cycles through all the stock and displays it for the user
 function dispStock(x)
 {
     console.log("Item ID" + " || " + "Product Name"+ " || " + "Price" + " || "  + "Quantity Available");
@@ -85,6 +95,7 @@ function dispStock(x)
  };
 };
 
+//Function that cycles through the available stock for each product and returns the items that have less than 5 available in stock
 function lowStock(x)
 {
   console.log("==============Items Low on Inventory==============")
@@ -98,8 +109,10 @@ function lowStock(x)
   };
 };
 
+//Function that allows the manager to add more inventory to a specific item.
 function addInventory(x)
 {
+  //Prompts manager to input which item they want to restock and to specify how many items will be added to it
   inquirer.prompt([
     {
         type: "input",
@@ -112,8 +125,10 @@ function addInventory(x)
         message: "How many items would you like to add to the stock?"
     }
   ]).then(function(answer){
+    //Verifies if the selected item is valid and if the amount being added is more than 0.
      if(parseInt(answer.moreItems) < x.length+1 && parseInt(answer.moreItems) > 0)
      {
+       //If the selection is valid the function adds the current stock to the new stock and then updates that item's "stock_quantity" in the table.
        var updatedStock = x[answer.moreItems-1].stock_quantity + parseInt(answer.amount);
        connection.query("UPDATE products SET stock_quantity=? WHERE item_id =?",[updatedStock,answer.moreItems], function(err) {
          if (err) throw err;
@@ -122,14 +137,17 @@ function addInventory(x)
         })
      }else
      {
+      //If the option isn't valid then the user is asked to select a new item
       console.log("That item is not in stock. Please select another item.");
       addInventory(x);
      }
   })
 };
 
+//Function that adds a whole new item to the products table in the database
 function addProduct()
 {
+  //Asks the user for the new product information that is required by the database
   inquirer.prompt([
     {
         type: "input",
@@ -152,6 +170,8 @@ function addProduct()
         message: "How many are you adding to the inventory?"
       },
   ]).then(function(answers){
+
+    //Once all the questions are answered, the function will insert the new item directly into the table of products
      var addItem = {product_name:answers.name, department_name:answers.department, price:parseFloat(answers.price), stock_quantity:parseInt(answers.stock)};
      connection.query("INSERT INTO products SET ?",addItem,function(err){
          if (err) throw err;
